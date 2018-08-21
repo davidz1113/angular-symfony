4f5a8ac55b5c31ec9242d58351b3cd21f6f90432
David Andres <d.carvajal_13@hotmail.com>
2018-08-21 11:48:09 -0500

    creando menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login.component';
 import { RegisterComponent } from './components/register.component';
-
+import {routing,appRoutingProviders} from './app.routing';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,12 @@ import { RegisterComponent } from './components/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
